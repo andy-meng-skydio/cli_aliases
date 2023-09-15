80f6de85a1dff2992cbc0f74010d0af93da33616
andy-meng-skydio <andy.meng@skydio.com>
2023-09-15 13:01:45 -0700

    adds command completion

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 sudo apt install bash-completion
 
+git submodule update --init
+
+cp complete-alias/complete_alias ~/.complete_alias
+
 if [ -f ~/.bashrc ]; then
   echo "found .bashrc file"
   if grep -Fq /cli_aliases ~/.bashrc
@@ -8,6 +12,7 @@ if [ -f ~/.bashrc ]; then
   else
       echo "adding cli_aliases to .bashrc"
       echo . $PWD"/cli_aliases" >> ~/.bashrc
+      echo 'complete -F _complete_alias "${!BASH_ALIASES[@]}"' >> ~/.bashrc
   fi
 else
   echo ".bashrc not found"
