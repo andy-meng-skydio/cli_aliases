84a7d27577a7a8511ec3fd56f3f4f275f362b73d
andy-meng-skydio <andy.meng@skydio.com>
2023-09-21 16:34:17 -0700

    adds yubikey provisioning

---
+++ README.md
@@ -0,0 +1,5 @@
+## bash aliases: git and some aircam stuff
+
+clone and run install.sh
+
+requires 1password-cli for yubikey provisioning from 1password
--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-sudo apt install bash-completion
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+sudo apt install bash-completion expect
 git submodule update --init
-
 cp complete-alias/complete_alias ~/.complete_alias
+chmod +x $SCRIPT_DIR/yub_script
 
 if [ -f ~/.bashrc ]; then
   echo "found .bashrc file"
@@ -12,6 +13,7 @@ if [ -f ~/.bashrc ]; then
   else
       echo "adding cli_aliases to .bashrc"
       echo . $PWD"/cli_aliases" >> ~/.bashrc
+      echo "export CLI_ALIAS_FOLDER="$SCRIPT_DIR >> ~/.bashrc
       echo 'complete -F _complete_alias "${!BASH_ALIASES[@]}"' >> ~/.bashrc
   fi
 else
